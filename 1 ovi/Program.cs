40de6ddf989bbe5dfc915332e1418c8721715763
utta <111429116+Uttamoosa@users.noreply.github.com>
2024-01-15 13:55:28 +0200

    nojuu

--- 1 ovi/Program.cs
+++ 1 ovi/Program.cs
@@ -1,2 +1,36 @@
-﻿int ovitila = 0;
+﻿enum ovitila { auki, kiinni, lukossa};
 
+class oviholmeli
+{
+    static void Main(string[] args)
+    {
+        ovitila tila = ovitila.auki;
+        Console.WriteLine(tila);
+        while (true) {
+
+            string toiminto = Console.ReadLine();
+               
+            if (toiminto == "avaa" && tila == ovitila.kiinni)
+            {
+                tila = ovitila.auki;
+                Console.WriteLine("on auki");
+            }
+
+            else if (toiminto == "sulje" && tila == ovitila.auki)
+            {
+                tila = ovitila.kiinni;
+                Console.WriteLine("on kiinni");
+            }
+
+            else if (toiminto == "lukitse" && tila == ovitila.kiinni)
+            {
+                tila = ovitila.lukossa;
+                Console.WriteLine("on lukissa");
+            }
+            else
+            {
+                Console.WriteLine("eipä timinu");
+            }
+        }
+    }
+}
