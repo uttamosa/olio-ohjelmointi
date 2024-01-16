166d1b4cb6209f51eefc111de78c2f394d20fbae
utta <111429116+Uttamoosa@users.noreply.github.com>
2024-01-16 11:28:13 +0200

    nojuu

---
+++ 2 ruoka/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+class holmeli
+{
+    enum raaka { kana, jäätelö, puolukka }
+    enum lisukkeet { konvehtirasia, ruoho, kananmuna}
+    enum kastike { tomaatti, jauheliha, suklaakastike}
+
+    struct annos
+    {
+        public string raaka;
+        public string lisuke;
+        public string 
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("mikä raakaaine syömiseen");
+        string raaka = Console.ReadLine();
+
+        Console.WriteLine("mikä raakaaine syömiseen");
+        string lisuke = Console.ReadLine();
+
+        Console.WriteLine("mikä raakaaine syömiseen");
+        string kastike = Console.ReadLine();
+    }
+}
