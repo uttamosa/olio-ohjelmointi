279da13be7d544ef91bab19993c7dbdfbe4f7da5
utta <153897790+uttamosa@users.noreply.github.com>
2024-01-18 09:40:00 +0200

    nojuu

---
+++ 3 nuoli/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _3_nuoli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        class Nuoli
+        {
+            kärkityyppi kärki;
+            int pituus;
+            perätyyppi perä;
+        }
+
+        enum kärkityyppi
+        {
+            kivi,
+            teräs,
+            timantti
+        }
+
+        enum perätyyppi
+        {
+            kanasulka,
+            kotkasulka,
+            dodosulka
+        }
+    }
+}
